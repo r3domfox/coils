ed0b5dfed0dacaf57d155aaf6e1e058402c1e4ad
Dominic.Fox <dominic.fox@r3.com>
2024-01-11 15:38:54 +0000

    Set up test configuration so the application context can start

---
+++ src/main/kotlin/com/r3/coils/CoilsApplication.kt
@@ -0,0 +1,11 @@
+package com.r3.coils
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoilsApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoilsApplication>(*args)
+}
---
+++ src/test/kotlin/com/r3/coils/CoilsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.r3.coils
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoilsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
